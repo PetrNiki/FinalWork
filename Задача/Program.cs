5da09683f2871ff796253e14c7772a7a87c67efd
PetrNiki <nikitenkopv@yandex.ru>
2022-09-05 06:53:55 +0300

    Задача Решение

---
+++ Задача/Program.cs
@@ -0,0 +1,17 @@
+﻿void methodArray (string [] array)
+{
+     string[] newarr = new string[array.Length];
+     for (int i=0; i<array.Length; i++)
+     {      
+        if(array[i].Length<=3)
+        {
+        newarr[i]=array[i];;
+        Console.WriteLine(newarr[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
+
+string [] array = {"hello", "2", "world", ":-)"};
+
+methodArray(array);
